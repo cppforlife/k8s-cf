c5ced8cb58878e666f0ba413af4cb8391c7b8c45
Dmitriy Kalinin <cppforlife@gmail.com>
2019-12-12 17:50:00 -0500

    updates for latest ytt build

--- hack/template-build.sh
+++ hack/template-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 ytt -f config \
-	-f config/_ytt_lib/cc/config/build.yml \
-	-f config/_ytt_lib/uaa/config/build.yml \
 	-f hack/build-from-source.yml \
 	-f user-provided/values-with-db.yml
